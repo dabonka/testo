d8d13e43debc46824ce81b62b18591ee93a8e181
Roman Belik <roman.belik@chisw.com>
2019-05-17 18:03:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const list = [
+  {
+    title: 'React1',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'React2',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  },
+  {
+    title: 'React3',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  },
+  {
+    title: 'React4',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 3,
+  },
+  {
+    title: 'React5',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 4,
+  },
+  {
+    title: 'Redux6',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 5,
+  },
+];
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      list,
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.list.map(item => {
+          return (
+            <div key={item.objectID}>
+              <p>{item.title}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
+
 
 export default App;
